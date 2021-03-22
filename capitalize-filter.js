d8f3198cb5789b47c9d62b868fcbe040a849758b
Vance-M <myveryvills@yahoo.com>
2021-03-22 16:33:04 -0700

    commented out for loop in capAndFilter andbrought back array methods

--- capitalize-filter.js
+++ capitalize-filter.js
@@ -1,25 +1,25 @@
-const capAndFilter = (strings) => { 
-    const newStrings = [];
-    for(let string of strings) {
+// const capAndFilter = (strings) => { 
+//     const newStrings = [];
+//     for(let string of strings) {
 
-        // string = string.toString();
-        console.log(string);
-        string = string.toUpperCase();
-        if(!string.startsWith('F')){
-            newStrings.push(string);
-        }
+//         // string = string.toString();
+//         console.log(string);
+//         string = string.toUpperCase();
+//         if(!string.startsWith('F')){
+//             newStrings.push(string);
+//         }
 
             
-    }
+//     }
 
-    return (newStrings);
-};
-
-// const capAndFilter = (strings) => { 
-//     const allCapStrings = strings.map(string => string.toUpperCase());
-//     const newstrings = allCapStrings.filter(string => !(string.charAt(0).startsWith('F')));
-//     return (newstrings);
+//     return (newStrings);
 // };
+
+const capAndFilter = (strings) => { 
+    const allCapStrings = strings.map(string => string.toUpperCase());
+    const newstrings = allCapStrings.filter(string => !(string.charAt(0).startsWith('F')));
+    return (newstrings);
+};
 
 
 module.exports = capAndFilter;
